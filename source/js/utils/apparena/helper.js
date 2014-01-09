122821a815bd7501e242e3c65cea8c522460e312
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-09 15:53:13 +0100

    GNR-16 optimized config and translation helper with debug information on missing parameters

--- source/js/utils/apparena/helper.js
+++ source/js/utils/apparena/helper.js
@@ -8,12 +8,14 @@ define(['underscore'], function (_) {
                 translate, param, text;
 
             if (num === 0) {
+                _.debug.error('No parameters set to translate!');
                 return '';
             }
 
             translate = _.filter(_.aa.locale, {l_id: arguments[0]});
 
             if (typeof translate !== 'object' || typeof translate[0] === 'undefined') {
+                _.debug.warn('Please create a new translation in app wizard for ' + arguments[0].toString());
                 return arguments[0].toString();
             }
 
@@ -23,7 +25,6 @@ define(['underscore'], function (_) {
                 delete arguments[0];
 
                 param = _.values(arguments);
-
                 text = _.sprintf(text, param);
             }
 
@@ -32,13 +33,18 @@ define(['underscore'], function (_) {
 
         // return config value or config key on given identifier from global app arena variable
         '__c': function (identifier, key) {
-            if (typeof key === 'undefined') {
-                key = 'value';
-            }
+            key = key || 'value';
 
-            if (typeof _.aa.config[identifier][key] === 'undefined') {
+            if (_.isEmpty(identifier)) {
+                _.debug.error('No config parameters set!');
                 return false;
             }
+
+            if (!_.isObject(_.aa.config[identifier]) || !_.isString(_.aa.config[identifier][key])) {
+                _.debug.warn('Please create a new config value in app wizard for ' + identifier);
+                return false;
+            }
+
             return _.aa.config[identifier][key];
         },
 
