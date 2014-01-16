8a7d04a2d3329de0d43931229cae85a715e38d06
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-16 13:42:27 +0100

    GNR-16 cleanup

--- source/doc/mysql/app.sql
+++ source/doc/mysql/app.sql
@@ -7,13 +7,3 @@ CREATE TABLE `app_settings` (
 )
     COLLATE ='utf8_general_ci'
     ENGINE =InnoDB;
-
-
-CREATE TABLE `modules` (
-    `id`         INT(11) UNSIGNED    NOT NULL AUTO_INCREMENT,
-    `name`       VARCHAR(255)        NOT NULL,
-    `is_enabled` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-    PRIMARY KEY (`id`)
-)
-    COLLATE ='utf8_general_ci'
-    ENGINE =InnoDB;
